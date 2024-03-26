aef0f730dd3c5666db9cbc223df1f8fda26413ea
Vishnu <128610832+Vishnuu011@users.noreply.github.com>
2024-03-26 11:56:11 +0000

    thired commit

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages,setup
 from typing import List
-
 
 HYPEN_E_DOT='-e .'
 
-
 def get_requirements(file_path:str)->List[str]:
-
+    '''
+    this function will return the list of requirements
+    '''
     requirements=[]
 
     with open(file_path) as file_obj:
-        requirements=file_obj.readline()
+        requirements=file_obj.readlines()
         requirements=[req.replace('\n',"") for req in requirements]
 
         if HYPEN_E_DOT in requirements:
@@ -19,7 +19,7 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-name='ml_project',
+name='mlproject',
 version='0.0.2',
 author='vishnuu001',
 author_email='vishnurrajeev@gmail.com',
---
+++ src/mlproject/pipelines/training_pipeline.py
@@ -0,0 +1,4 @@
+from src.mlproject.logger import logging
+
+
+logging.info("first log")
